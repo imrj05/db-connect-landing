10c66b00348dbf10a776821fa5f46126e1eae756
work-rjkashyap <work.rjkashyap05@gmail.com>
2026-04-06 23:27:34 +0530

    refactor: sanitize Appwrite environment variables by stripping surrounding quotes

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,9 +2,12 @@ import { Client, Account, Databases } from "appwrite";
 
 export const client = new Client();
 
+const endpoint = (process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || "https://cloud.appwrite.io/v1").replace(/^["']|["']$/g, '');
+const projectId = (process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || "YOUR_PROJECT_ID").replace(/^["']|["']$/g, '');
+
 client
-    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || "https://cloud.appwrite.io/v1")
-    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || "YOUR_PROJECT_ID");
+    .setEndpoint(endpoint)
+    .setProject(projectId);
 
 export const account = new Account(client);
 export const databases = new Databases(client);
